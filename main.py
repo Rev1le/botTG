847d2674885c0185c54e035f67c8db5d48d5f666
Rev1le <r.nikiy@yandex.ru>
2021-12-03 14:07:06 +0500

    Первый коммит

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from json import dumps, loads#
 import time
 import urllib3
 
-TOKEN = <YOUR TOKEN>
+TOKEN = "<YOUR TOKEN>"
 
 ATRIBUT = "limit=3&offset=-3&allowed_updates=message"
 
@@ -21,6 +21,7 @@ ID = 172631146
 while True:
     global response 
     response = get(HTTP + "getUpdates?"+ ATRIBUT)
+    print('We have logged in as ')
     #a_list = list(json.loads(response).values())
     #a = json.dumps(response)
     #print(json.dumps(response.json(), sort_keys=True, indent=4))
@@ -52,9 +53,6 @@ while True:
                 S = ['mr.ДЕгенерал - это Рома','mr.Да - НЯ!! - это Даня Даниил наивысший','mr.Андрей (( - это Андрей','mr.SaSKEE - это Саня вот и всё']
                 resp = get(HTTP + f"sendMessage?chat_id=<chat_id>&text={S[i]}")
             #resp = get(HTTP + "sendMessage?chat_id=848986553&text=Без%20python%20не%20кошерно")
-
-
-
         print(d)
     time.sleep(0.2)
 
@@ -63,6 +61,3 @@ while True:
     
 #print(json_data)
 print(response.json())
-
-#while True:
-#    res = get(HTTP + "getUpdates")
